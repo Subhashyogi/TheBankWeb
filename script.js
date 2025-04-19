fb20b67ca586b33141977f8ffb4aa6d389dac571
Subhash Yogi <subhashyogi999@gmail.com>
2025-04-19 19:40:14 +0530

    updated

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 // const BASE_URL = import.meta.env.VITE_BACKEND_URL;
-const BASE_URL = window.BASE_URL;
+const BASE_URL = window.BACKEND_URL;
 // DOM elements
 const header = document.querySelector(".header");
 const btnScrollTo = document.querySelector(".btn--scroll-to");
@@ -291,7 +291,7 @@ const nextHandler = (e) => {
     pin: pin.value,
   };
   axios
-    .post(`${BASE_URL}user/create`, formData)
+    .post(`${BASE_URL}/user/create`, formData)
     .then((success) => {
       if (success.data.status === 1) {
         console.log("user added successfully");
